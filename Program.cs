a3eb7a414ec91dc54bea96ec2543d58f826a0ce7
Findsen <chr.findsen@gmail.com>
2017-02-16 21:17:41 +0100

    Upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // nested object
+            // Starting the webhostbuilder from the configuration file named StartUp
+            var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseStartup<Startup>()
+            .Build();
+
+            host.Run();
+
+        }
+    }
+}
